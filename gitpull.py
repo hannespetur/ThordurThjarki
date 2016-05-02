9adf01a3f7af16774395bb880804383c04eb5edb
Hannes Petur Eggertsson <hannespetur@gmail.com>
2016-05-02 22:24:53 +0000

    [Bugfix] Bot was not pulling from git. Maybe this fixes the problem.

--- gitpull.py
+++ gitpull.py
@@ -1,6 +1,7 @@
 import git
 
 async def gitpull(client, message):
-  g = git.cmd.Git()
+  await client.send_message(message.channel, "Starting to pull changes from Github...")
+  g = git.cmd.Git(".")
   g.pull()
   await client.send_message(message.channel, "Latest changes have been pull from git.")
